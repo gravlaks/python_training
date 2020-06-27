17f9fc3cb6df117044e1b0a0e1483916f011dc0e
Torstein Eliassen <32542379+gravlaks@users.noreply.github.com>
2020-06-27 16:09:12 +0200

    add states

--- FSM.py
+++ FSM.py
@@ -1,9 +1,10 @@
 from Timer import Timer
 class FSM:
 
-    def __init__(self, periodic_functions, one_time_functions):
+    def __init__(self, periodic_functions, one_time_functions, init_state="init"):
         self.periodic_functions = {}
         self.one_time_functions = {}
+        self.current_state = init_state
 
 
          
@@ -11,9 +12,16 @@ class FSM:
         for fnc, parameters in periodic_functions.items():
             period = parameters["period"] 
             args = parameters["args"]
+            entry_states = parameters["entry_states"]
+            end_state = parameters["end_state"]
+
             self.periodic_functions[fnc] = {}
+
+
             self.periodic_functions[fnc]["timer"] = Timer(period)
             self.periodic_functions[fnc]["args"] = args
+            self.periodic_functions[fnc]["entry_states"] = entry_states
+            self.periodic_functions[fnc]["end_state"] = end_state
 
         for fnc, parameters in one_time_functions.items():
             execution_time = parameters["execution_time"]
@@ -29,14 +37,19 @@ class FSM:
 
         while True:
 
-
             for fnc, parameters in self.periodic_functions.items():
+                
                 args = parameters["args"]
                 timer = parameters["timer"]
-                
-                if timer.timeout():
-                    fnc(*args)
-                    timer.reset()
+                entry_states = parameters["entry_states"] 
+                end_state = parameters["end_state"]
+
+                if self.current_state in entry_states:
+                    if timer.timeout():
+                        print("Current state", self.current_state)
+                        fnc(*args)
+                        timer.reset()
+                        self.current_state = end_state
 
             for fnc, parameters in self.one_time_functions.items():
                 args = parameters["args"]
--- test_fsm.py
+++ test_fsm.py
@@ -2,7 +2,7 @@ from FSM import FSM
 def main():
     print("Test program")
 
-    periodic_functions = {print_hello_world:{"args": (), "period": 1}, print_every_five:{"args": (), "period": 5}, print_anything: {"args": ("hello every 0.5 seconds", ), "period": 0.5}}
+    periodic_functions = {print_hello_world:{"args": (), "period": 1, "entry_states": ("second state"), "end_state": "second state"}, print_every_five:{"args": (), "period": 5, "entry_states": ("init", "second state"), "end_state": "second state"}, print_anything: {"args": ("hello every 0.5 seconds", ), "period": 0.5, "entry_states": ("init"), "end_state": "init"}}
 
     one_time_functions = {print_anything:{"args": ("hello only when 10 seconds have gone", ), "execution_time": 10}}
     fsm = FSM(periodic_functions, one_time_functions)
